14facca4436f1972df310aad99cac9925670f8dc
Xantorohara <xantorohara@gmail.com>
2016-02-21 21:14:22 +0400

    modify example

--- examples/XantoTM1637_Seconds_Counter/XantoTM1637_Seconds_Counter.ino
+++ examples/XantoTM1637_Seconds_Counter/XantoTM1637_Seconds_Counter.ino
@@ -1,19 +1,23 @@
+/**
+ * Display time in seconds with blinked colon.
+ */
 #include <XantoI2C.h>
 
-#include "XantoTM1637.h"
+#include <XantoTM1637.h>
 
-const uint8_t PIN_DISPLAY_CLK = 8;
-const uint8_t PIN_DISPLAY_DIO = 9;
+const uint8_t PIN_DISPLAY_CLK = 5;
+const uint8_t PIN_DISPLAY_DIO = 6;
 
 XantoTM1637 display(PIN_DISPLAY_CLK, PIN_DISPLAY_DIO);
 
 void setup() {
 }
 
-
 int i = 0;
 void loop() {
   display.displayTime(i);
+  delay(500);
+  display.displayTime(i, false);
   delay(500);
   i++;
 }
